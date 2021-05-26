6afb1ef862e8860239fc3edd14b295cd1cd7fc0e
javier.hernandez <javier.hernandez@meaningfuldata.eu>
2021-05-26 15:09:39 +0200

    fix: final test

--- testApi.py
+++ testApi.py
@@ -17,15 +17,6 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
-def pretty(d, indent=0):
-    for key, value in d.items():
-        print('\t' * indent + str(key))
-        if isinstance(value, dict):
-            pretty(value, indent + 1)
-        else:
-            print('\t' * (indent + 1) + str(value))
-
-
 def main():
     pass
 
