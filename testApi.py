17dbff99d34e7af709d0c0015f228e23975a3be6
javier.hernandez <javier.hernandez@meaningfuldata.eu>
2021-05-26 15:17:08 +0200

    fix: final test 2

--- testApi.py
+++ testApi.py
@@ -1,22 +1,3 @@
-import logging
-
-logger = logging.getLogger("logger")
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
-# formatter = logging.Formatter("%(message)s")
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
-
-
 def main():
     pass
 
