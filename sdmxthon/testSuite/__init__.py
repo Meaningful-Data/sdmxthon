ed14f16452d0c40cbac999f8496ae4c29c757068
javier.hernandez <javier.hernandez@meaningfuldata.eu>
2021-05-26 14:54:53 +0200

    fix: test for workflow

--- sdmxthon/testSuite/__init__.py
+++ sdmxthon/testSuite/__init__.py
@@ -1,7 +1,1 @@
-import os
-import sys
 
-sys.path.append(os.path.join(os.getcwd(), 'model'))
-sys.path.append(os.path.join(os.getcwd(), 'api'))
-sys.path.append(os.path.join(os.getcwd(), 'parsers'))
-sys.path.append(os.path.join(os.getcwd(), 'utils'))
