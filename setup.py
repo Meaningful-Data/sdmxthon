7690508cfafa2457e650b0feea9fc7f01116c8b4
javier.hernandez <javier.hernandez@meaningfuldata.eu>
2022-12-22 17:41:53 +0100

    Version 1.4. Fixed VTL mapping and fixed small bugs on reading.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description_content_type='text/x-rst',
     packages=setuptools.find_packages(exclude=["testSuite"]),
     include_package_data=True,
-    version='1.3',
+    version='1.4',
     license='Apache 2.0',
     license_files='license.txt',
     author='MeaningfulData',
