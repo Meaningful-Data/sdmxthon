0c2372f3d4a80e9bd8df7331032037b70a7e4459
marina.velasco <marina.velasco@meaningfuldata.eu>
2023-01-04 11:17:06 +0100

    reading_improvements: code for getting dataflows list and downloading specific dataflow from ECB api

--- testApi.py
+++ testApi.py
@@ -62,7 +62,11 @@ def return_agencies():
                 'EUROSTAT': {'name': 'Eurostat',
                              'code': 'ESTAT',
                              'api_base_url': 'https://ec.europa.eu/eurostat/api/dissemination'
-                             }
+                             },
+                'ECB': {'name': 'ecb.europa',
+                        'code': 'ECB',
+                        'api_base_url': 'https://sdw-wsrest.ecb.europa.eu'
+                        }
                 }
     return agencies
 
@@ -231,6 +235,100 @@ def EUROSTAT_return_dataflow_data(name='EUROSTAT',
     # print(data)
 
 
+def ECB_return_dataflows(name='ECB'):
+    agencies = return_agencies()
+    agency_id = agencies[name]['code']
+
+    base_url = agencies[name]['api_base_url']
+    version = 'all'
+    resource_id = 'all'
+    references = 'none'
+    detail = 'full'
+
+    url_bis = base_url + '/service/dataflow/' + agency_id + '/' + resource_id + '/' + version + '?references=' + references + '&detail=' + detail
+
+    message = read_sdmx(url_bis, validate=False)
+
+    dataflows = message.payload['Dataflows'].values()
+    list_dataflows = []
+    for i in dataflows:
+        i: DataFlowDefinition
+        info = {'id': i.id,
+                'unique_id': i.unique_id,
+                'name': i.name,
+                'description': i.description,
+                'version': i.version}
+        list_dataflows.append(info)
+
+    return list_dataflows
+
+
+def ECB_return_dataflow_data(name='ECB',
+                             dataflow_unique_id='ESTAT:HLTH_EHIS_SK4E(1.0)',
+                             start_period=None, end_period=None,
+                             updated_after=None,
+                             first_n_observations=None,
+                             last_n_observations=None,
+                             dimension_at_observation=None,
+                             detail='full',
+                             key='all',
+                             provider_ref='all',
+                             references=None):
+    agencies = return_agencies()
+
+    agencies_names = agencies.keys()
+    if name not in agencies_names:
+        return {"status code": 404,
+                "message": "This agency name does not match with any available agency"}
+    agency_id = agencies[name]['code']
+    base_url = agencies[name]['api_base_url']
+
+    dataflows = ECB_return_dataflows(name)
+
+    list_dataflows_unique_id = []
+    for df in dataflows:
+        list_dataflows_unique_id.append(df['unique_id'])
+
+    if dataflow_unique_id not in list_dataflows_unique_id:
+        return {"status code": 404,
+                "message": "This unique id does not match with any available dataset"}
+    else:
+        index = list_dataflows_unique_id.index(dataflow_unique_id)
+
+    dataflow = dataflows[index]
+    dataflow_version = dataflow['version']
+    dataflow_id = dataflow['id']
+    url_df = base_url + '/service/data/' + agency_id + ',' + dataflow_id + ',' + dataflow_version + '/' + key + '/' + provider_ref + '?'
+    # agency_id and dataflow_id could be 'all' and dataflow_version could be 'all' and 'latest'
+    if start_period is not None:
+        url_df = url_df + 'startPeriod=' + start_period + '&'
+    if end_period is not None:
+        url_df = url_df + 'endPeriod=' + end_period + '&'
+    if updated_after is not None:
+        url_df = url_df + 'updatedAfter=' + updated_after + '&'
+    if first_n_observations is not None:
+        url_df = url_df + 'firstNObservations=' + first_n_observations + '&'
+    if last_n_observations is not None:
+        url_df = url_df + 'lastNObservations=' + last_n_observations + '&'
+    if dimension_at_observation is not None:
+        url_df = url_df + 'dimensionAtObservation=' + dimension_at_observation + '&'
+    if references is not None:
+        url_df = url_df + 'references=' + detail
+    url_df = url_df + 'detail=' + detail
+
+    req = rq.get(url_df)
+    if req.status_code != 200:
+        return {"status code": req.status_code, "message":
+            req.text.split('<com:Text>')[1].split('</com:Text>')[0]}
+    else:
+        file = f"development_files/{dataflow_id}_data.xml"
+        with open(file, "wb") as fl:
+            fl.write(req.content)
+
+    # data = read_sdmx(url_df, validate=False)
+    # print(data)
+
+
 def main():
     ## EXAMPLES
     # message = read_sdmx(data_file1, validate=False)
@@ -238,13 +336,26 @@ def main():
     # print(message.payload['ESTAT:EI_BSCO_M$DEFAULTVIEW(1.0)']
     # .data.to_csv('test.csv', index=False))
 
+    # Para devolver agencias
+    # message_def = return_agencies()
+
+    # Para listar los dataflows de cada agencia
+    # message_def = BIS_return_dataflows(name='BIS')
+    # message_def = EUROSTAT_return_dataflows(name='EUROSTAT')
+    # message_def = ECB_return_dataflows(name='ECB')
+
+    # Para devolver data de un dataflow en concreto
     # message_def = BIS_return_dataflow_data(name='BIS',
     #                                        dataflow_unique_id='BIS:WS_CBPOL_D(1.0)',
     #                                        first_n_observations='10')
 
-    message_def = EUROSTAT_return_dataflow_data(name='EUROSTAT',
-                                                dataflow_unique_id='ESTAT:HLTH_EHIS_SK4E(1.0)',
-                                                first_n_observations='1')
+    # message_def = EUROSTAT_return_dataflow_data(name='EUROSTAT',
+    #                                             dataflow_unique_id='ESTAT:HLTH_EHIS_SK4E(1.0)',
+    #                                             first_n_observations='1')
+
+    message_def = ECB_return_dataflow_data(name='ECB',
+                                           dataflow_unique_id='ECB:AME(1.0)',
+                                           first_n_observations='1')
 
     print(message_def)
 
