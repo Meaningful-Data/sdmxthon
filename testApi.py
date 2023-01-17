8f12f43ae125ed7fef338c4f8d126bb8cc74ee38
marina.velasco <marina.velasco@meaningfuldata.eu>
2023-01-17 09:49:27 +0100

    reading_improvements: code for getting dataflows list and downloading specific dataflow from ILO api

--- testApi.py
+++ testApi.py
@@ -55,7 +55,7 @@ url_bis = "https://stats.bis.org/api/v1/dataflow/BIS/all/latest?references=none&
 
 
 def return_agencies():
-    agencies = {'BIS': {'name': 'BIS Stats',
+    agencies = {'BIS': {'name': 'Bank for International Settlements',
                         'code': 'BIS',
                         'api_base_url': 'https://stats.bis.org/api/v1'
                         },
@@ -63,9 +63,13 @@ def return_agencies():
                              'code': 'ESTAT',
                              'api_base_url': 'https://ec.europa.eu/eurostat/api/dissemination'
                              },
-                'ECB': {'name': 'ecb.europa',
+                'ECB': {'name': 'European Central Bank',
                         'code': 'ECB',
                         'api_base_url': 'https://sdw-wsrest.ecb.europa.eu'
+                        },
+                'ILO': {'name': 'International Labour Organization',
+                        'code': 'ILO',
+                        'api_base_url': 'https://www.ilo.org/sdmx/rest'
                         }
                 }
     return agencies
@@ -329,6 +333,101 @@ def ECB_return_dataflow_data(name='ECB',
     # print(data)
 
 
+def ILO_return_dataflows(name='ILO'):
+    agencies = return_agencies()
+    agency_id = agencies[name]['code']
+
+    base_url = agencies[name]['api_base_url']
+    version = 'all'
+    resource_id = 'all'
+    references = 'none'
+    detail = 'full'
+
+    url_bis = base_url + '/dataflow/' + agency_id + '/' + resource_id + '/' + version + '?references=' + references + '&detail=' + detail
+
+    message = read_sdmx(url_bis, validate=False)
+    print(message)
+
+    dataflows = message.payload['Dataflows'].values()
+    list_dataflows = []
+    for i in dataflows:
+        i: DataFlowDefinition
+        info = {'id': i.id,
+                'unique_id': i.unique_id,
+                'name': i.name,
+                'description': i.description,
+                'version': i.version}
+        list_dataflows.append(info)
+
+    return list_dataflows
+
+
+def ILO_return_dataflow_data(name='ILO',
+                             dataflow_unique_id='ILO:DF_CLD_TPOP_SEX_AGE_GEO_NB(1.0)',
+                             key='all', start_period=None, end_period=None,
+                             first_n_observations=None,
+                             last_n_observations=None,
+                             dimension_at_observation=None,
+                             updated_after=None,
+                             include_history='false',
+                             detail='full'):
+    agencies = return_agencies()
+    print(agencies)
+    agencies_names = agencies.keys()
+    if name not in agencies_names:
+        return {"status code": 404,
+                "message": "This agency name does not match with any available agency"}
+    agency_id = agencies[name]['code']
+    base_url = agencies[name]['api_base_url']
+
+    dataflows = ILO_return_dataflows(name)
+
+    list_dataflows_unique_id = []
+    for df in dataflows:
+        list_dataflows_unique_id.append(df['unique_id'])
+
+    if dataflow_unique_id not in list_dataflows_unique_id:
+        return {"status code": 404,
+                "message": "This unique id does not match with any available dataset"}
+    else:
+        index = list_dataflows_unique_id.index(dataflow_unique_id)
+
+    dataflow = dataflows[index]
+
+    dataflow_version = dataflow['version']
+    dataflow_id = dataflow['id']
+
+    url_df = base_url + '/data/' + dataflow_id + '/' + key + '/' + agency_id + '?'
+    if start_period is not None:
+        url_df = url_df + 'startPeriod=' + start_period + '&'
+    if end_period is not None:
+        url_df = url_df + 'endPeriod=' + end_period + '&'
+    if first_n_observations is not None:
+        url_df = url_df + 'firstNObservations=' + first_n_observations + '&'
+    if last_n_observations is not None:
+        url_df = url_df + 'lastNObservations=' + last_n_observations + '&'
+    if dimension_at_observation is not None:
+        url_df = url_df + 'dimensionAtObservation=' + dimension_at_observation + '&'
+    if updated_after is not None:
+        url_df = url_df + 'updatedAfter=' + updated_after + '&'
+    if include_history != 'false':
+        url_df = url_df + 'includeHistory=' + include_history + '&'
+
+    url_df = url_df + 'detail=' + detail
+
+    req = rq.get(url_df)
+    if req.status_code != 200:
+        return {"status code": req.status_code, "message":
+            req.text.split('<com:Text>')[1].split('</com:Text>')[0]}
+    else:
+        file = f"development_files/{dataflow_id}_data.xml"
+        with open(file, "wb") as fl:
+            fl.write(req.content)
+
+    # data = read_sdmx(url_df, validate=False)
+    # print(data)
+
+
 def main():
     ## EXAMPLES
     # message = read_sdmx(data_file1, validate=False)
@@ -343,6 +442,7 @@ def main():
     # message_def = BIS_return_dataflows(name='BIS')
     # message_def = EUROSTAT_return_dataflows(name='EUROSTAT')
     # message_def = ECB_return_dataflows(name='ECB')
+    # message_def = ILO_return_dataflows(name='ILO')
 
     # Para devolver data de un dataflow en concreto
     # message_def = BIS_return_dataflow_data(name='BIS',
@@ -353,9 +453,12 @@ def main():
     #                                             dataflow_unique_id='ESTAT:HLTH_EHIS_SK4E(1.0)',
     #                                             first_n_observations='1')
 
-    message_def = ECB_return_dataflow_data(name='ECB',
-                                           dataflow_unique_id='ECB:AME(1.0)',
-                                           first_n_observations='1')
+    # message_def = ECB_return_dataflow_data(name='ECB',
+    #                                        dataflow_unique_id='ECB:AME(1.0)',
+    #                                        first_n_observations='1')
+
+    message_def = ILO_return_dataflow_data(name='ILO',
+                                           dataflow_unique_id='ILO:DF_CLD_TPOP_SEX_AGE_GEO_NB(1.0)')
 
     print(message_def)
 
