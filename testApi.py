32b956f077f217f329f365fafa3bfd0dd1fcdd2a
Antonio Olleros <antonio.olleros@meaningfuldata.eu>
2023-06-29 19:02:54 +0200

    Fixed open issue 29

--- testApi.py
+++ testApi.py
@@ -1,7 +1,11 @@
 # flake8: noqa
+import sdmxthon
 
 def main():
-    pass
+    message_data = sdmxthon.read_sdmx('https://stats.bis.org/api/v1/data/BIS,WS_OTC_DERIV2,1.0/all/all?lastNObservations=3&detail=full')
+    dataset = message_data.content['BIS:WS_OTC_DERIV2(1.0)']
+    # dataset.structure = sdmxthon.read_sdmx('https://stats.bis.org/api/v1/dataflow/BIS/WS_OTC_DERIV2/1.0?references=all&detail=full').content['DataStructures']['BIS:BIS_DER(1.0)']
+    print(dataset.structural_validation())
 
 
 if __name__ == "__main__":
