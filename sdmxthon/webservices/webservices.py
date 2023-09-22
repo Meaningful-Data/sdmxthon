f44b5cdfc0d6cc0b3c1c2f1e725be50a81915808
Antonio Olleros <antonio.olleros@meaningfuldata.eu>
2023-09-22 12:19:44 +0200

    Pandas vitaminizado

--- sdmxthon/webservices/webservices.py
+++ sdmxthon/webservices/webservices.py
@@ -5,6 +5,8 @@
 
 from abc import ABC
 
+import pandas as pd
+
 from sdmxthon.api.api import read_sdmx
 from sdmxthon.model.definitions import DataFlowDefinition
 from sdmxthon.parsers.read import read_xml
@@ -69,6 +71,7 @@ class SdmxWebServiceConnection(ABC):
     def get_data(self, flow, **kwargs):
         """Returns a message with the data"""
         url = self.get_data_url(flow, **kwargs)
+        print(url)
         message = read_sdmx(url, validate=False)
         return message
 
@@ -89,6 +92,54 @@ class SdmxWebServiceConnection(ABC):
         url = self.get_constraints_url(kwargs)
         message = read_sdmx(url, validate=False)
         return message
+
+    def get_pandas_with_names(self, flow, **kwargs):
+        """Returns a Pandas DataFrame with the data"""
+        def _create_codelist_dataframe(codelist, concept_name):
+            codelist_list = []
+            for id, code in codelist.items.items():
+                item = {'id': id}
+                for lang_code, strings in code.name.items():
+                    item[f"{concept_name}-{lang_code}"] = strings['content']
+                codelist_list.append(item)
+            
+            return pd.DataFrame(codelist_list)
+
+        def _generate_insight_dict(metadata_payload):
+            result = {}
+            codelists = metadata_payload['Codelists']
+            structure = metadata_payload['DataStructures']
+            if len(structure) !=  1:
+                raise Exception('One structure expected')
+            structure = list(structure.values())[0]
+            for id, component in structure.dimension_descriptor.components.items():
+                codelist=component.representation.codelist
+                if codelist:
+                    codelist = _create_codelist_dataframe(codelist, component.id)
+                result[id] = {'name': component.concept_identity.name, 'codelist': codelist}
+            return result
+
+
+        def _generate_final_df_and_concepts_name(data, metadata_payload):
+            insight_dict = _generate_insight_dict(metadata_payload)
+
+            concepts_names = {}
+            for code, component in insight_dict.items():
+                concepts_names[code] = component['name']
+                if component['codelist'] is not None:
+                    data = data.merge(component['codelist'], left_on=code, right_on='id', how='inner')
+                    data.drop(columns=['id_x', 'id_y'], inplace=True, errors='ignore')
+            data.to_csv('data.csv')
+            return data, concepts_names
+
+        metadata = self.get_data_flow(flow, references='descendants')
+        message = self.get_data(flow, **kwargs)
+        data = list(message.payload.values())[0].data
+        df, concepts_name = _generate_final_df_and_concepts_name(data, metadata.payload)
+
+        df.to_csv('data.csv')
+
+        return df, concepts_name
 
     @staticmethod
     def get_sdmxthon_code(url):
--- testApi.py
+++ testApi.py
@@ -1,6 +1,44 @@
 # flake8: noqa
-from sdmxthon.webservices import webservices
+from sdmxthon.webservices import webservices, query_builder
+import pandas as pd
 
+def create_codelist_dataframe(codelist, concept_name):
+    codelist_list = []
+    for id, code in codelist.items.items():
+        item = {'id': id}
+        for lang_code, strings in code.name.items():
+            item[f"{concept_name}-{lang_code}"] = strings['content']
+            print(item)
+        codelist_list.append(item)
+    
+    return pd.DataFrame(codelist_list)
+
+def generate_insight_dict(metadata_payload):
+    result = {}
+    codelists = metadata_payload['Codelists']
+    structure = metadata_payload['DataStructures']
+    if len(structure) !=  1:
+        raise Exception('One structure expected')
+    structure = list(structure.values())[0]
+    for id, component in structure.dimension_descriptor.components.items():
+        codelist=component.representation.codelist
+        if codelist:
+            codelist = create_codelist_dataframe(codelist, component.id)
+        result[id] = {'name': component.concept_identity.name, 'codelist': codelist}
+    return result
+
+
+def generate_final_df_and_concepts_name(data, metadata_payload):
+    insight_dict = generate_insight_dict(metadata_payload)
+
+    concepts_names = {}
+    for code, component in insight_dict.items():
+        concepts_names[code] = component['name']
+        if component['codelist'] is not None:
+            data = data.merge(component['codelist'], left_on=code, right_on='id', how='inner')
+            data.drop(columns=['id_x', 'id_y'], inplace=True, errors='ignore')
+    data.to_csv('data.csv')
+    return data, concepts_names
 
 def main():
     # bis_ws = webservices.BisWs()
@@ -15,21 +53,23 @@ def main():
     ws = webservices.IloWs()
     dataflow_id = 'DF_CLD_XCHL_SEX_AGE_STE_NB'
 
-    ws = webservices.BisWs()
-    dataflow_id = 'WS_CBPOL_D'
+    # ws = webservices.BisWs()
+    # dataflow_id = 'WS_CBPOL_D'
 
-    ws = webservices.EuroStatWs()
-    dataflow_id = 'MAR_QG_QM_EWHD'
+    # ws = webservices.EuroStatWs()
+    # dataflow_id = 'MAR_QG_QM_EWHD'
 
-    ws = webservices.EcbWs()
-    dataflow_id = 'AME'
+    # ws = webservices.EcbWs()
+    # dataflow_id = 'AME'
+    # ws.get_pandas_with_names(dataflow_id, last_n_observations=1)
+    print(ws.get_data_url(dataflow_id, last_n_observations=1))
 
     # print(ws.get_data_url(dataflow_id, last_n_observations=1))
     # print(ws.get_data_flow_url(dataflow_id, references='descendants'))
-    data = ws.get_data(dataflow_id, last_n_observations=1)
-    print(data.payload)
-    metadata = ws.get_data_flow(dataflow_id, references='descendants')
-    print(metadata.payload)
+    # data = ws.get_data(dataflow_id, last_n_observations=1)
+    # print(data.payload)
+    # metadata = ws.get_data_flow(dataflow_id, references='descendants')
+    # print(metadata.payload)
 
 
     
