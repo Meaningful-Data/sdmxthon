3bf863746611a7bf410dff7524a96e195aee2cf5
javier.hernandez <javier.hernandez@meaningfuldata.eu>
2023-10-26 18:35:32 +0200

    master. Removed static path.

--- sdmxthon/docs/conf.py
+++ sdmxthon/docs/conf.py
@@ -73,8 +73,3 @@ html_theme_options = {
     'includehidden': False,
     'titles_only': False
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
